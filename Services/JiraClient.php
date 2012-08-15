881972ea551fbd85533e09229009646fdc5b5f43
Mathieu GOULIN <mathieu.goulin@gadz.org>
2012-08-15 15:38:21 +0200

    add component support in jira bundle

--- Services/JiraClient.php
+++ Services/JiraClient.php
@@ -10,17 +10,19 @@ class JiraClient
     private $port;
     private $wsdl;
     private $password;
+    private $logger;
 
-    public function __construct($server, $port, $wsdl, $user, $password)
+    public function __construct($logger, $server, $port, $wsdl, $user, $password)
     {
         $this->server   = $server;
         $this->port     = $port;
         $this->wsdl     = $wsdl;
         $this->user     = $user;
         $this->password = $password;
+        $this->logger   = $logger;
     }
 
-    public function createIssue($project, $subject, $body)
+    public function createIssue($project, $subject, $body, $extraField = array(), $components = array())
     {
         $soap = new \SoapClient ("http://" . $this->server . ":" . $this->port . $this->wsdl,
             array ("style" => SOAP_RPC,"use" => SOAP_ENCODED,"soap_version" => SOAP_1_1,"uri" => "urn:myWS"));
@@ -31,12 +33,22 @@ class JiraClient
             $rIssue['project'] = $project;
             $rIssue['type'] = '2';
             $rIssue['summary'] = $subject;
+            foreach($components as $componentId => $componentName)
+            {
+               $rIssue['components'][] = array('id' => $componentId, 'name' => $componentName);
+            }
             $rIssue['description'] = $body;
             $rIssue['priority'] = '4';
             $rIssue['reporter'] = $this->user;
+            foreach($extraField as $fieldId => $fieldValue)
+            {
+                $rIssue['customFieldValues'][] = array('customfieldId' => $fieldId, 'values' => array($fieldValue));
+            }
             $result = $soap->createIssue ($auth, $rIssue);
             $soap->logout ($this->user);
+            $this->logger->info("Create issue " . $result->key . ": " . $subject);
         }
         return $result->key;
    }
 }
+
